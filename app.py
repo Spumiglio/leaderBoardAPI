f70debd6443708638cf56165767377eafd83e8ae
Spumiglio <mattiaburati@live.it>
2022-05-05 12:29:06 +0100

    fix

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import jsonify, make_response
 import sqlite3
 app = Flask(__name__)
 
-con = sqlite3.connect('G:\leaderBoardAPI\db.sqlite', check_same_thread=False)
-
 def checkUsername(username):
+    con = sqlite3.connect('db.sqlite', check_same_thread=False)
     cur = con.cursor()
     cur.execute('''SELECT * FROM user_scores WHERE username =:user;''', {"user":username})
     result = cur.fetchall()
     cur.close()
+    con.close()
     if len(result) == 0:
         return True
     else:
@@ -17,29 +17,36 @@ def checkUsername(username):
 
 @app.route('/createUser/<username>')
 def createUsername(username):
+    con = sqlite3.connect('db.sqlite', check_same_thread=False)
     if checkUsername(username):
         cur = con.cursor()
         resp = cur.execute('''INSERT INTO user_scores VALUES(?,?)''',(username,0)).fetchall()
         con.commit()
         cur.close()
+        con.close()
         if len(resp) == 0:
             return "OK"
         else:
             return "ERROR"
     else:
+        con.close()
         return "ALREADY USED"
 
 def getUserScore(username):
+    con = sqlite3.connect('db.sqlite', check_same_thread=False)
     cur = con.cursor()
     resp = cur.execute('''SELECT scores FROM user_scores WHERE username = ?''',(username,)).fetchall()
     cur.close()
+    con.close()
     return resp
 
 @app.route('/getScores')
 def getScores():
+    con = sqlite3.connect('db.sqlite', check_same_thread=False)
     cur = con.cursor()
     res = cur.execute('''SELECT * FROM user_scores ORDER BY scores DESC ''',).fetchall()
     cur.close()
+    con.close()
     string = ""
     for i in res:
         string += i[0]+"|"+str(i[1])+"\n"
@@ -48,6 +55,7 @@ def getScores():
 
 @app.route('/uploadScore/<username>/<score>')
 def uploadScore(username,score):
+    con = sqlite3.connect('db.sqlite', check_same_thread=False)
     cur = con.cursor()
     current = getUserScore(username)
     current = current[0]
@@ -55,13 +63,16 @@ def uploadScore(username,score):
         res = cur.execute('''UPDATE user_scores set scores = ? WHERE username = ?''', (score,username))
     con.commit()
     cur.close()
+    con.close()
     return "OK"
 
 @app.route('/resetPlayer/<username>')
 def resetPlayer(username):
+    con = sqlite3.connect('db.sqlite', check_same_thread=False)
     cur = con.cursor()
-    res = cur.execute('''DELETE FROM user_scores WHERE username = ?''', (username,)).fetchall()
+    res = cur.execute('''UPDATE user_scores set scores = 0 WHERE username = ?''', (username,)).fetchall()
     cur.close()
+    con.close()
     if len(res) == 0:
         return "OK"
     else:
